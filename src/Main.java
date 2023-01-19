0639c91011023be373d28dde83f58a1a6106d9c8
Vally <murkhonovbatoha@gmail.com>
2023-01-20 04:07:10 +0800

    Скелет

--- src/Main.java
+++ src/Main.java
@@ -11,35 +11,44 @@ public class Main {
         task9();
         task10();
     }
+    //for(int i = 0; i < 10; i = i + 1){
+    //    System.out.println("Итерация цикла " + i);
 
     public static void task1() {
-        System.out.println("Задача 1");
+        System.out.println((char) 27 + "[35mЗадача 1" + (char)27 + "[0m");
+        for(int i = 1; i <= 10; i = i + 1){
+            System.out.println("число " + i);
+        }
     }
     public static void task2() {
-        System.out.println("Задача 2");
+        System.out.println((char) 27 + "[35mЗадача 2" + (char)27 + "[0m");
+        for (int a = 10; a >= 1; a = a - 1) {
+            System.out.println("Число " + a);
+        }
     }
     public static void task3() {
-        System.out.println("Задача 3");
+        System.out.println((char) 27 + "[35mЗадача 3" + (char)27 + "[0m");
+        
     }
     public static void task4() {
-        System.out.println("Задача 4");
+        System.out.println((char) 27 + "[35mЗадача 4" + (char)27 + "[0m");
     }
     public static void task5() {
-        System.out.println("Задача 5");
+        System.out.println((char) 27 + "[35mЗадача 5" + (char)27 + "[0m");
     }
     public static void task6() {
-        System.out.println("Задача 6");
+        System.out.println((char) 27 + "[35mЗадача 6" + (char)27 + "[0m");
     }
     public static void task7() {
-        System.out.println("Задача 7");
+        System.out.println((char) 27 + "[35mЗадача 7" + (char)27 + "[0m");
     }
     public static void task8() {
-        System.out.println("Задача 8");
+        System.out.println((char) 27 + "[35mЗадача 8" + (char)27 + "[0m");
     }
     public static void task9() {
-        System.out.println("Задача 9");
+        System.out.println((char) 27 + "[35mЗадача 9" + (char)27 + "[0m");
     }
     public static void task10() {
-        System.out.println("Задача 10");
+        System.out.println((char) 27 + "[35mЗадача 10" + (char)27 + "[0m");
     }
 }
